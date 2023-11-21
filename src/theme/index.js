71328c5afdcf13301c73603733d30d6b65403989
Sudarshan P <psudarshan37@gmail.com>
2023-11-21 09:15:04 +0530

    refactor: change theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,4 @@
+// https://colorhunt.co/palette/2c33332e4f4f0e8388cbe4de
 const getDesignTokens = (mode) => ({
   palette: {
     mode,
@@ -5,20 +6,41 @@ const getDesignTokens = (mode) => ({
       ? {
           type: "light",
           primary: {
-            main: "#fafafa",
+            light: "#93b1db",
+            dark: "#0c1d36",
+            main: "#1D3557",
+            contrastText: "#e4e6eb",
           },
           secondary: {
-            main: "#26a27b",
+            light: "#f5e8e6",
+            dark: "#a34e41",
+            main: "#F5CAC3",
+            contrastText: "#050505",
           },
         }
       : {
           type: "dark",
           primary: {
-            main: "#26a27b",
+            main: "#CBE4DE",
+            light: "#e6edeb",
+            dark: "#a6bdb7",
+            contrastText: "#000000",
           },
           secondary: {
-            main: "#fafafa",
+            main: "#E4CBD1",
+            light: "#e3d5d9",
+            dark: "#b09ba0",
+            contrastText: "#0D0D14",
           },
+          background: {
+            default: "#2C3333",
+            paper: "#2E4F4F",
+          },
+          text: {
+            primary: "#EBEBEE",
+            secondary: "#C1C1C1",
+          },
+          devider: "#2E4F4F",
         }),
   },
 });
